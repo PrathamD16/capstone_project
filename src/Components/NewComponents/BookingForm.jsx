c4b50b5c82d7c150010ad1192d8101b7bc930e89
PrathamD16 <pratham.doke.1602@gmail.com>
2024-10-20 20:04:54 +0530

    Booking Form Complete

--- src/Components/NewComponents/BookingForm.jsx
+++ src/Components/NewComponents/BookingForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import Grid from '@mui/material/Grid2'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { UserEmailContext } from "../../Context/CredContext";
 import { Button, Stack, TextField } from '@mui/material'
 import Radio from '@mui/material/Radio';
@@ -14,6 +14,7 @@ const BookingForm = () => {
   const { fid } = useParams()
 
   const { byEmail, signedIn } = useContext(UserEmailContext);
+  const nav = useNavigate()
 
   const [loading, setLoading] = useState(false)
   const [cost, setCost] = useState(0)
@@ -23,11 +24,12 @@ const BookingForm = () => {
   const [cname, setCname] = useState("");
   const [contact, setContact] = useState(0);
   const [age, setAge] = useState(0);
-  const [gender, setGender] = useState("");
+  const [gender, setGender] = useState("female");
 
   const [list, setList] = useState([])
 
   const [btnDisable, setBtnDisable] = useState(false)
+
 
 
   useEffect(() => {
@@ -36,15 +38,16 @@ const BookingForm = () => {
         const res = await axios.get(
           ` http://localhost:5000/flight-service/api/search/flight/${fid}`
         );
-        setCost(res.data.cost)
         setAvaSeats(res.data.total_seats - res.data.booked_seats)
+        setCost(res.data.cost)
       } catch (e) {
         console.log(e)
       }
     }
     getFlightDetail()
     console.log(avaSeats)
-  }, [fid, list])
+    console.log("Cost is: ", cost)
+  }, [fid])
 
   useEffect(() => {
     setBtnDisable(!(JSON.stringify(contact).trim().length === 12 && contact > 0 && age > 0))
@@ -75,6 +78,11 @@ const BookingForm = () => {
       .then((res) => {
         console.log(res.data)
         setList([...list, res.data])
+
+        // setCname("")
+        // setContact("")
+        // setAge("")
+        // setGender("female")
       })
       .catch((err) => {
         console.log(err);
@@ -85,7 +93,6 @@ const BookingForm = () => {
   const deleteHandler = (index) => {
     console.log(`deleted ${index}`)
     setList(list.filter((booking) => booking.bid !== index))
-
     axios
       .delete(
         `http://localhost:4000/passenger-service/api/deleteBooking/${index}`
@@ -96,69 +103,112 @@ const BookingForm = () => {
       .catch((err) => {
         console.log(err);
       });
+  }
 
+  const paymentHandler = () => {
+    nav("/user/showbooking")
   }
 
   return (
     <Grid container spacing={2}>
-      <Grid flex={7}>
-        <div className='flex items-center justify-center h-screen bg-gray-100'>
-          <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+      <Grid item flex={6} display="flex" flexDirection="column" justifyContent="space-between">
+        <div className="flex items-center justify-center bg-gray-100">
+          <div className="bg-white p-8 rounded-lg shadow-lg w-96 my-2">
             <h3 className="text-xl font-semibold text-center mb-4">Passenger Detail Form</h3>
-            <form className='space-y-5' onSubmit={submitHandler}>
-              <div className='flex'>
-                <TextField onChange={e => setCname(e.target.value)} type='text' value={cname} className='flex-1' label="Name" required />
+            <form className="space-y-5" onSubmit={submitHandler}>
+              <div className="flex">
+                <TextField
+                  onChange={(e) => setCname(e.target.value)}
+                  type="text"
+                  value={cname}
+                  className="flex-1"
+                  label="Name"
+                  required
+                />
               </div>
-              <div className='flex'>
-                <TextField onChange={e => setContact(e.target.value)} type='number' value={contact} className='flex-1' label="Contact" required />
+              <div className="flex">
+                <TextField
+                  onChange={(e) => setContact(e.target.value)}
+                  type="number"
+                  value={contact}
+                  className="flex-1"
+                  label="Contact"
+                  required
+                />
               </div>
-              <div className='flex'>
-                <TextField onChange={e => setAge(e.target.value)} type='number' value={age} className='flex-1' label="Age" required />
+              <div className="flex">
+                <TextField
+                  onChange={(e) => setAge(e.target.value)}
+                  type="number"
+                  value={age}
+                  className="flex-1"
+                  label="Age"
+                  required
+                />
               </div>
               <RadioGroup
                 row
                 aria-labelledby="demo-radio-buttons-group-label"
                 defaultValue="female"
                 name="radio-buttons-group"
-                onClick={e => setGender(e.target.value)}
+                onClick={(e) => setGender(e.target.value)}
               >
                 <FormControlLabel value="female" control={<Radio />} label="Female" />
                 <FormControlLabel value="male" control={<Radio />} label="Male" />
                 <FormControlLabel value="other" control={<Radio />} label="Other" />
               </RadioGroup>
-              <div className='flex'>
-                <Button disabled={btnDisable} type='submit' className='flex-1' variant='contained'>Submit</Button>
+              <div className="flex">
+                <Button disabled={btnDisable} type="submit" className="flex-1" variant="contained">
+                  Submit
+                </Button>
               </div>
             </form>
           </div>
         </div>
       </Grid>
-      <Grid flex={5}>
-        <div className='flex'>
-          <Button disabled={list.length <= 0 ? true : false} variant='contained' color='success' className='flex-1'>Proceed for payment</Button>
-        </div>
-        {
-          list.length <= 0 ? `List is empty` : <>
-            {
-              list.map((booking, _i) => {
+
+      <Grid className="bg-gray-100 p-2" item flex={6} display="flex" flexDirection="column" justifyContent="space-between">
+        <div>
+          {list.length <= 0 ? (
+            <div className='flex justify-center'>
+              <p className='text-xl mt-10'>No passengers in the list</p>
+            </div>
+          ) : (
+            <div className="h-[300px] overflow-y-auto mt-4">
+              {list.map((booking, _i) => {
                 return (
-                  <Stack className='bg-slate-200 text-sm px-2 rounded-md py-1 my-2' key={_i}>
-                    <div className='flex justify-between items-center'>
-                      <div>
-                        <p>Name: {booking.cname}</p>
-                        <p>Age: {booking.age}</p>
-                        <p>Gender: {booking.gender}</p>
+                  <Stack className="bg-slate-200 text-sm rounded-md p-4 my-2 hover:bg-gradient-to-l from-blue-200 to-slate-500 hover:text-white transition-all duration-500 ease-in-out transform hover:scale-[1.02]" key={_i}>
+                    <div className="flex justify-between items-start gap-4 flex-wrap">
+                      <div className="flex-1 min-w-0">
+                        <p>Name: <span className="font-semibold break-words">{booking.cname.toUpperCase()}</span></p>
+                        <p>Age: <span className="font-semibold">{booking.age}</span></p>
+                        <p>Gender: <span className="font-semibold">{booking.gender.toUpperCase()}</span></p>
                       </div>
-                      <Button onClick={() => deleteHandler(booking.bid)} color="error"><DeleteForeverOutlined /></Button>
+                      <div className="flex-shrink-0">
+                        <Button onClick={() => deleteHandler(booking.bid)} color="error">
+                          <DeleteForeverOutlined />
+                        </Button>
+                      </div>
                     </div>
                   </Stack>
-                )
-              })
-            }
-          </>
-        }
+
+                );
+              })}
+            </div>
+          )}
+        </div>
+
+        <div className="flex items-center justify-around">
+          <Button onClick={paymentHandler} disabled={list.length <= 0} variant="contained" color="success" className="">
+            Proceed for payment
+          </Button>
+          <div className='bg-green-600 text-white py-2 px-5 rounded-lg'>
+            <p>Total : {list.length * cost} Rs</p>
+          </div>
+        </div>
       </Grid>
     </Grid>
+
   )
 }
 
--- src/Components/UserComponents/ShowUserBooking.jsx
+++ src/Components/UserComponents/ShowUserBooking.jsx
@@ -2,17 +2,22 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { UserEmailContext } from "../../Context/CredContext";
 import BookingTile from "./BookingTile";
+import CircularProgress from '@mui/material/CircularProgress';
+
 const ShowUserBooking = () => {
   const [count, setCount] = useState(0);
   const [bookingList, setBookingList] = useState([]);
   const { byEmail } = useContext(UserEmailContext);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const getData = async () => {
+      setLoading(true)
       const res = await axios.get(
         `http://localhost:4000/passenger-service/api/getBookingByEmail?email=${byEmail}`
       );
       setBookingList(res.data);
+      setLoading(false)
     };
     getData();
     console.log(bookingList.length)
@@ -20,19 +25,24 @@ const ShowUserBooking = () => {
 
   return (
     <div className="space-y-5">
-      {bookingList.length > 0 ? (
-        bookingList.map((book, _i) => {
-          return (
-            <div key={_i} className="px-5">
-              <BookingTile trigger={setCount} book={book} />
+      {
+        loading ? <div className="flex items-center justify-center h-screen">
+          <CircularProgress color="inherit" />
+        </div> : <>
+          {bookingList.length > 0 ? (
+            bookingList.map((book, _i) => {
+              return (
+                <div key={_i} className="px-5">
+                  <BookingTile trigger={setCount} book={book} />
+                </div>
+              );
+            })
+          ) : (
+            <div className="flex items-center h-screen justify-center">
+              <p className="text-3xl">No Booking Records</p>
             </div>
-          );
-        })
-      ) : (
-        <div className="flex items-center h-screen justify-center">
-          <p className="text-3xl">No Booking Records</p>
-        </div>
-      )}
+          )}</>
+      }
     </div>
   );
 };
